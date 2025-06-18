4e42af6049a268fc0ef3f46c75e21e050b1a78c9
Matvey Ivanov <swim.enthusiast@gmail.com>
2025-06-18 13:33:09 -0400

    csv

--- Int. Python/packing.py
+++ Int. Python/packing.py
@@ -1,1 +1,19 @@
 import csv
+data = [
+  ['Item', 'Quantity'],
+  ['Blender', 2],
+  ['Posters', 30],
+  ['Shoes', 2]
+]
+
+try:
+    with open('packing_list.csv','r') as file:
+# work with files here
+        csv_reader = csv.reader(file)
+        for row in csv_reader:
+            print(row)
+except FileNotFoundError:
+    print("Packing list file not found. Creating a new one")
+    with open('packing_list.csv', 'w', newline='') as file:
+        csv_writer = csv.writer(file)
+        csv_writer.writerows(data)  
